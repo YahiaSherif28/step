f7b4126737dcada33f919a584cf8958affd976e6
Yahia <yahiaabdelbar@gmail.com>
2020-08-02 16:47:58 +0200

    Fix linter errors

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,69 +14,71 @@
 
 package com.google.sps;
 
-import java.util.Collection;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     // timeIsOccupied is true if someone in the request attendees has an event in that moment
-    boolean[] timeIsOccupied = new boolean[TimeRange.END_OF_DAY+1];
+    boolean[] timeIsOccupied = new boolean[TimeRange.END_OF_DAY + 1];
     Collection<String> requestAttendees = request.getAttendees();
 
-    for(Event event: events) {
+    for(Event event : events) {
       Set<String> eventAttendees = event.getAttendees();
-      for(String s: requestAttendees) {
+      for(String s : requestAttendees) {
         // if someone in our request is in this event,
         // make the range of the event [start,end) true in the timeIsOccupied[]
-        if(eventAttendees.contains(s)) {
+        if (eventAttendees.contains(s)) {
           TimeRange eventRange = event.getWhen();
-          for(int i = eventRange.start(); i < eventRange.end(); ++i) {
+          for (int i = eventRange.start(); i < eventRange.end(); ++i) {
             timeIsOccupied[i] = true;
           }
           break;
         }
       }
     }
-    return findResultOfQuery (timeIsOccupied, request.getDuration());
+    return findResultOfQuery(timeIsOccupied, request.getDuration());
   }
   /** 
-   * finds continous intervals of false in timeIsOcuppied[]
-   * with lengths greater than or equal to durationOfRequest
-   * and returns them as a Collection<TimeRange>
+   * finds continous intervals of false in timeIsOcuppied[] with lengths greater than or equal to
+   * durationOfRequest and returns them as a Collection<TimeRange>
    */
-  private Collection<TimeRange> findResultOfQuery (boolean[] timeIsOccupied, long durationOfRequest) {
+  private Collection<TimeRange> findResultOfQuery (
+      boolean[] timeIsOccupied, long durationOfRequest) {
     Collection<TimeRange> result = new ArrayList<TimeRange>();
     int start = 0;
     int end = 1;
-    while(end < timeIsOccupied.length) {
+    while (end < timeIsOccupied.length) {
       // if start is occupied move forward to search for a valid start
-      if(timeIsOccupied[start]) {
+      if (timeIsOccupied[start]) {
         start++;
-        end = start+1;
+        end = start + 1;
         // start is not occupied (valid) but end is occupied
         // meaning that there is range of falses [start,end)
-      } else if(timeIsOccupied[end]) {
+      } else if (timeIsOccupied[end]) {
         TimeRange validRange = TimeRange.fromStartEnd(start, end, false);
         // check that the duration of the range found is enough for the request
-        if(durationOfRequest <= validRange.duration())
+        if (durationOfRequest <= validRange.duration()) {
           result.add(validRange);
+        }
         start = end + 1;
         end = start + 1;
         // !timeIsOccupied[start] && !timeIsOccupied[end]
         // start is not occupied (valid) and
         // we haven't reached the end of the interval yet
-      } else { 
+      } else {
         end++;
       }
     }
     // To handle the case when a suffix of the array is false,
     // range [start,timeIsOccupied.length) is all false
-    if(!timeIsOccupied[timeIsOccupied.length-1]) {
+    if (!timeIsOccupied[timeIsOccupied.length - 1]) {
       TimeRange validRange = TimeRange.fromStartEnd(start, timeIsOccupied.length, false);
-      if(durationOfRequest <= validRange.duration())
+      if (durationOfRequest <= validRange.duration()) {
         result.add(validRange);
+      }
     }
     return result;
   }
